9ce2ba0cd3c5965ba28a235c87183dbad3863ecc
sunnyaiml <sunnyvishwakarma9372@gmail.com>
2024-11-29 16:17:30 +0530

    upgrade in build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@
 curl --proto 'https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-# Install Python dependencies
+# Verify Rust installation
+rustc --version
+
+# Upgrade pip and install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
